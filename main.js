c711af26a486769dba31e0de03e67005382fa426
gsgsjssvsv1249 <sandeepky102010@gmail.com>
2025-08-19 06:55:45 +0530

    Update main.js

--- main.js
+++ main.js
@@ -149,7 +149,7 @@ imageUpload.addEventListener('change', async (event) => {
       const formData = new FormData();
       formData.append('image', file);
 
-      const res = await fetch('http://localhost:3000/upload', {
+      const res = await fetch('production-asia-southeast1-eqsg3a.railway-registry.com/upload', {
         method: 'POST',
         body: formData
       });
